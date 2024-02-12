ed699c61f1fa3d54a88d3d2ad0d12631780e07fc
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-11 19:59:58 -0800

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import json
+from json import dump
+from json import load
 """module that defines the FileStorage class"""
 
 
@@ -10,25 +12,25 @@ class FileStorage:
 
     def all(self):
         """public instance method"""
-        return self.__objects
+        return FileStorage__objects
 
     def new(self, obj):
         """public instance method"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """public instance method"""
         a = {}
-        for key, obj in self.__objects.items():
-            a[key] = obj.to_obj()
-        with open(self.__file_path, 'w') as file:
+        for key, obj in FileStorage.__objects.items():
+            a[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
             json.dump(a, file)
 
     def reload(self):
         """public instance method"""
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, encoding="utf-8") as file:
                 self.__objects = json.load(file)
                 for key, value in self.__objects.items():
                     class_name, obj_id = key.split('.')
