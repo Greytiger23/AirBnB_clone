2bf0997863782d5694fed1ba20c53ad964bed569
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-13 19:25:39 -0800

    airbnb

--- console.py
+++ console.py
@@ -7,6 +7,19 @@ class HBNBCommand(cmd.Cmd):
     """represents the class"""
     prompt = "(hbnb) "
 
+    def do_quit(self, arg):
+        """exit program"""
+        return True
+
+    def do_EOF(self, arg):
+        """exit program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """do nonthing"""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
