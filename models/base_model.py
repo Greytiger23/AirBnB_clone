2f6fd700a37aa42f751d8cd40bf90ad85f735aad
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-13 19:05:33 -0800

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ class BaseModel:
     """represents the class"""
     def __init__(self, *args, **kwargs):
         """public instance method"""
+        from models import storage
         if kwargs:
             del kwargs["__class__"]
             for key, value in kwargs.items():
@@ -20,23 +21,23 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """public method"""
-        return "[{}] ({]) {}".format(
+        return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """public instance method"""
-        self.updated_at = datetime.now()
+        from models import storage
+        updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """punlic instance method"""
         obj_dict = {}
         obj_dict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if isinstance(value, datetime):
-                obj_dict[key] = value.isoformat()
-            else:
-                obj_dict[key] = value
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
