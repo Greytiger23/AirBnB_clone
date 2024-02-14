ad7966200306d08d18e6e57ba04c092fe5cdb6c4
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-13 18:08:13 -0800

    airbnb clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,7 @@ class FileStorage:
 
     def all(self):
         """public instance method"""
-        return FileStorage__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """public instance method"""
@@ -25,16 +25,17 @@ class FileStorage:
         for key, obj in FileStorage.__objects.items():
             a[key] = obj.to_dict()
         with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
-            json.dump(a, file)
+            dump(a, file)
 
     def reload(self):
         """public instance method"""
         try:
             with open(FileStorage.__file_path, encoding="utf-8") as file:
-                self.__objects = json.load(file)
-                for key, value in self.__objects.items():
-                    class_name, obj_id = key.split('.')
-                    class_obj = globals()[class_name]
-                    self.__objects[key] = class_obj(**value)
+                s = load(file)
+                for value in s.values():
+                    class_name = value['__class__']
+                    if isinstance(class_name, str) and type(
+                            eval(class_name) == type:
+                        self.new(eval(class_name)(**value))
         except FileNotFoundError:
             pass
