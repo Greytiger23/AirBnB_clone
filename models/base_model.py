987debd51b30c7ca234526210c8a437daaa73b14
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-13 19:07:26 -0800

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,6 @@ class BaseModel:
     """represents the class"""
     def __init__(self, *args, **kwargs):
         """public instance method"""
-        from models import storage
         if kwargs:
             del kwargs["__class__"]
             for key, value in kwargs.items():
@@ -21,7 +20,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """public method"""
@@ -30,9 +28,7 @@ class BaseModel:
 
     def save(self):
         """public instance method"""
-        from models import storage
         updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """punlic instance method"""
